95073207cf10c96db25726fb8bba0d6d195e2ab6
Lauri Himanen <lauri.himanen@gmail.com>
2024-07-31 13:50:43 +0300

    Updated.

--- src/nomad_bayesian_optimization/example_uploads/__init__.py
+++ src/nomad_bayesian_optimization/example_uploads/__init__.py
@@ -1,24 +1,22 @@
-from nomad.config.models.plugins import AppEntryPoint
-from nomad.config.models.ui import App, Column, Columns, FilterMenu, FilterMenus
+from nomad.config.models.plugins import ExampleUploadEntryPoint
+from nomad.utils import strip
 
+getting_started = ExampleUploadEntryPoint(
+    title='Getting started with Bayesian optimization',
+    description=strip("""
+    Example upload that contains a Jupyter notebook demonstrating the basics of
+    Bayesian optimization.
+    """),
+    path='example_uploads/getting_started',
+    category='Lab automation',
+)
 
-myexampleupload = AppEntryPoint(
-    name='MyApp',
-    description='App defined using the new plugin mechanism.',
-    app=App(
-        label='MyApp',
-        path='myapp',
-        category='simulation',
-        columns=Columns(
-            selected=['entry_id'],
-            options={
-                'entry_id': Column(),
-            },
-        ),
-        filter_menus=FilterMenus(
-            options={
-                'material': FilterMenu(label='Material'),
-            }
-        ),
-    ),
+advanced = ExampleUploadEntryPoint(
+    title='Getting started with Bayesian optimization',
+    description=strip("""
+    Example upload that contains a Jupyter notebook demonstrating the basics of
+    Bayesian optimization.
+    """),
+    url='https://nomad-lab.eu/prod/v1/docs/assets/nomad-oasis.zip',
+    category='Lab automation',
 )
